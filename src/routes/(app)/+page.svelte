c0441aa668de4956285cf4e56aab535808686fe3
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-08-23 23:29:56 +0100

    reverse some url errors

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
-	$: next_page = $page.data.page + 1;
-	// let data: any;
-	// onMount(async()=>{
-	//      data = await(await fetch("https://api.consumet.org/anime/gogoanime/recent-episodes")).json()
-	// })
+	// $: next_page = $page.data.page + 1;
 </script>
 
 <pre class="pre">
         {JSON.stringify($page, null, 2)}
 </pre>
 
-<form action="." method="get">
+<!-- <form action="." method="get">
 	<input hidden name="page" value={next_page} type="text" />
 	<button class="btn">Next</button>
-</form>
+</form> -->
--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -3,10 +3,10 @@ import type { PageLoad } from "./$types";
 
 
 export const load: PageLoad = async ({ url }) => {
-    const page = Number(url.searchParams.get("page")) || 1
-    const animes = await gogo.fetchRecentEpisodes(page)
+    // const page = Number(url.searchParams.get("page")) || 1
+    const animes = await gogo.fetchRecentEpisodes()
     return {
         animes,
-        page
+        // page
     }
 }
