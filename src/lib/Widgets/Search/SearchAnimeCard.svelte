8f02e433495b1100d74c587fdb4aa4106b610bc6
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-09-10 16:00:06 +0100

    New Search Design. new design for the realtime searching. make it responsive. all good.

--- src/lib/Widgets/Search/SearchAnimeCard.svelte
+++ src/lib/Widgets/Search/SearchAnimeCard.svelte
@@ -5,12 +5,8 @@
 	export let data: any;
 </script>
 
-<!-- <pre>
-    {JSON.stringify(data, null, 2)}
-</pre> -->
-
 <section
-	class="grid max-h-[520px] lg:max-h-[700px] grid-cols-[repeat(auto-fill,minmax(140px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-2 overflow-y-scroll"
+	class="grid px-2 max-h-[520px] lg:max-h-[700px] grid-cols-[repeat(auto-fill,minmax(140px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(360px,1fr))] gap-2 overflow-y-scroll"
 >
 	{#each data?.results as anime}
 		<a
@@ -20,7 +16,7 @@
 			}}
 			href="/anime?id={anime.id}"
 		>
-			<div class="flex flex-col hover:variant-glass-surface rounded-2xl p-2">
+			<div class="flex lg:hidden flex-col hover:variant-glass-surface rounded-2xl p-2">
 				<div
 					style="background-image: url({anime.image});"
 					class="relative rounded-xl shadow-xl w-full h-[15rem] lg:h-[17rem] bg-center bg-cover bg-no-repeat"
@@ -36,6 +32,33 @@
 					<span class="font-bold">
 						{anime.title}
 					</span>
+				</div>
+			</div>
+			<div class="hidden lg:flex flex-row hover:variant-glass-surface rounded-2xl p-2">
+				<div
+					style="background-image: url({anime.image});"
+					class="relative rounded-xl shadow-xl w-[22rem] h-[15rem] lg:h-[17rem] bg-center bg-cover bg-no-repeat"
+				/>
+
+				<div class="flex flex-col h-[15rem] lg:h-[17rem] w-full mt-2 ml-5 justify-center">
+					<div class="flex flex-row">
+						<span class="mr-1 font-bold">Year :</span>
+						<span class="rounded font-bold chip variant-glass-primary">
+							<span>{anime.releaseDate}</span>
+						</span>
+					</div>
+
+					<hr class="m-2 ml-0" />
+					<h1 class="h3 mt-2 font-bold">
+						{anime.title}
+					</h1>
+
+					<div class="flex flex-row w-full mt-auto mb-2">
+						<span class="mr-1 font-bold">Type :</span>
+						<span class="rounded ml-5 font-bold chip variant-glass-error">
+							<span>{anime.subOrDub}</span>
+						</span>
+					</div>
 				</div>
 			</div>
 		</a>
