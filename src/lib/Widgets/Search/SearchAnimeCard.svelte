8e3461dbd3865126fce09e2c150ab43652b52269
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-09-25 20:56:09 +0100

    PERFORMANCE: Increase Performance for the Realtime Search. Make it more responsive. new UI/UX design for the Card.

--- src/lib/Widgets/Search/SearchAnimeCard.svelte
+++ src/lib/Widgets/Search/SearchAnimeCard.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <section
-	class="grid px-2 max-h-[520px] lg:max-h-[700px] grid-cols-[repeat(auto-fill,minmax(140px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(360px,1fr))] gap-2 overflow-y-scroll"
+	class="grid px-2 py-2 max-h-[520px] lg:max-h-[700px] grid-cols-[repeat(auto-fill,minmax(230px,1fr))] md:grid-cols-[repeat(auto-fill,minmax(270px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(360px,1fr))] gap-2 overflow-y-scroll"
 >
 	{#each data?.results as anime}
 		<a
@@ -16,14 +16,14 @@
 			}}
 			href="/anime?id={anime.id}"
 		>
-			<div class="flex flex-col lg:flex-row hover:variant-glass-surface rounded-2xl p-2">
+			<div class="flex flex-row hover:variant-glass-surface rounded-2xl p-2">
 				<div
 					style="background-image: url({anime.image});"
 					class="relative rounded-xl shadow-xl w-[22rem] h-[15rem] lg:h-[17rem] bg-center bg-cover bg-no-repeat"
 				/>
 
 				<div class="flex flex-col h-[15rem] lg:h-[17rem] w-full mt-2 ml-5 justify-center">
-					<div class="hidden lg:flex flex-row">
+					<div class="flex flex-row">
 						<span class="mr-1 font-bold">Year :</span>
 						<span class="rounded font-bold chip variant-glass-primary">
 							<span>{anime.releaseDate}</span>
@@ -35,7 +35,7 @@
 						{anime.title}
 					</h1>
 
-					<div class="hidden lg:flex flex-row w-full mt-auto mb-2">
+					<div class="flex flex-row w-full mt-auto mb-2">
 						<span class="mr-1 font-bold">Type :</span>
 						<span class="rounded ml-5 font-bold chip variant-glass-error">
 							<span>{anime.subOrDub}</span>
