69a6389ead1127c1ae35314a81422dc0dda9df42
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-10-09 19:16:34 +0100

    Add Logo

--- src/lib/Components/Navigation/NavigationBar.svelte
+++ src/lib/Components/Navigation/NavigationBar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import LogoWrited from './LogoWrited.svelte';
 	import { AppBar } from '@skeletonlabs/skeleton';
 	import { GithubIcon, SearchTrigger, ThemeTrigger } from '$lib';
 	import Logo from './Logo.svelte';
@@ -10,16 +11,16 @@
 			<Logo />
 		</a>
 	</svelte:fragment>
-	<div class="relative flex items-center">
-		<a href="/explore" class="btn text-base font-extrabold hover:variant-soft-primary rounded-md">
-			Explore
+	<div class="relative flex justify-center items-center">
+		<a class="flex justify-center items-center px-[6px] lg:px-4 gap-4" href="/">
+			<LogoWrited />
 		</a>
 	</div>
 	<svelte:fragment slot="trail">
 		<div class="flex flex-row gap-4 items-center">
 			<ThemeTrigger />
 			<!-- <SearchTrigger /> -->
-		 <GithubIcon />	
+			<GithubIcon />
 		</div>
 	</svelte:fragment>
 </AppBar>
