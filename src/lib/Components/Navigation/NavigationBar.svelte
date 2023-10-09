7ac2e6f2e58af49ba5595684e3fc666cd16a3265
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-10-09 23:39:12 +0100

    remove github link from navbar. writed logo fix width.

--- src/lib/Components/Navigation/NavigationBar.svelte
+++ src/lib/Components/Navigation/NavigationBar.svelte
@@ -20,7 +20,7 @@
 		<div class="flex flex-row gap-4 items-center">
 			<ThemeTrigger />
 			<!-- <SearchTrigger /> -->
-			<GithubIcon />
+			<!-- <GithubIcon /> -->
 		</div>
 	</svelte:fragment>
 </AppBar>
