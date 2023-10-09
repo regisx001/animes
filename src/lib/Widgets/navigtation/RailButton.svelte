b1a97deee8cb935f1ab15af7ce74a2864a2aefa4
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-10-09 18:56:52 +0100

    Major Changes and configuration for the AppRail Navigation.

--- src/lib/Widgets/navigtation/RailButton.svelte
+++ src/lib/Widgets/navigtation/RailButton.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
-    export let href:string;
-    export let active:boolean = false;
-	export let title :string 
+	import RailEffect from './RailEffect.svelte';
+	export let href: string;
+	export let active: boolean = false;
+	export let title: string;
+	$: hovered = false;
 </script>
 
+{#if hovered}
+	<RailEffect />
+{/if}
+
 <a
+	on:focus={() => {}}
+	on:mouseover={() => {
+		hovered = true;
+	}}
+	on:mouseleave={() => {
+		hovered = false;
+	}}
 	{href}
 	type="button"
-    class:bg-secondary-100={active}
-	class="bg-secondary-100 {active ?"before:absolute before:-left-[0.15vw] before:z-10 before:h-6 before:w-1 before:rounded-full before:bg-primary-500":""}  btn btn-icon relative w-[76px] rounded-[15px] p-0"
+	class:bg-secondary-100={active}
+	class=" {active
+		? 'before:absolute before:-left-[0.15vw] before:z-10 before:h-6 before:w-1 before:rounded-full before:bg-primary-500'
+		: ''}  btn btn-icon relative w-[76px] rounded-[15px] p-0"
 	><div class="inline-grid">
 		<div class="absolute inset-0 flex flex-col items-center justify-center gap-2">
-			<slot/>
-			<span class:hidden={active} class="text-[1rem] font-semibold capitalize leading-[1rem]">{title}</span>
+			<slot />
+			<span class:hidden={active} class="text-[1rem] font-semibold capitalize leading-[1rem]"
+				>{title}</span
+			>
 		</div>
 	</div>
 </a>
+
+
