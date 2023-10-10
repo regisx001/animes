ad990975a6d7984b3cd330c4f1fd30cd3fe547bb
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-10-10 16:20:12 +0100

    fix grid column template for the small screens.

--- src/lib/Components/Pages/RecentAnimePage.svelte
+++ src/lib/Components/Pages/RecentAnimePage.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section
-	class="mx-[5%] lg:mx-[10%] animes grid-cols-[repeat(auto-fill,minmax(150px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(200px,1fr))]"
+	class="mx-[5%] lg:mx-[10%] animes grid-cols-[repeat(auto-fill,minmax(120px,1fr))] md:grid-cols-[repeat(auto-fill,minmax(200px,1fr))]"
 >
 	{#each data.results as anime}
 		<RecentAnimeCard {anime} />
--- src/lib/Widgets/RecentAnimeCard.svelte
+++ src/lib/Widgets/RecentAnimeCard.svelte
@@ -2,31 +2,14 @@
 	export let anime: any;
 </script>
 
-<article class="card card-hover relative inline-block">
+<article class="card card-hover max-h-96 h-full relative inline-block">
 	<a href="/episodes?id={anime.episodeId}">
 		<header class="card-header">
-			<div class="img rounded" style="background-image: url({anime.image});" />
+			<div class="h-48 md:h-64 rounded bg-cover" style="background-image: url({anime.image});" />
 		</header>
 	</a>
 
 	<section class="title text-center p-4 justify-center">
 		{anime.title.slice(0, 40)}
 	</section>
-
-	<!-- <div class="variant-filled-surface rounded absolute -top-0 font-bold -right-0 z-10">
-		<span class="chip variant-soft hover:variant-filled">
-			<span>Episode {anime.episodeNumber}</span>
-		</span>
-	</div> -->
 </article>
-
-<style>
-	.card {
-		max-height: 400px;
-		height: 100%;
-	}
-	.img {
-		height: 270px;
-		background-size: cover;
-	}
-</style>
