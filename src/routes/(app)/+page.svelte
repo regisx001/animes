5d93f7ca8273a7aeaf46a120567cbc3071318d70
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2024-04-11 20:51:34 +0000

    Performance: Improvement

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { gogo, RecentAnimePage, Carousel } from '$lib';
+	import { inview } from 'svelte-inview';
 	import { onMount } from 'svelte';
 
 	async function fetchAnimes(page: number = 1) {
@@ -10,6 +11,7 @@
 	$: curr_page = 1;
 	$: data = {};
 	$: is_ready = false;
+	$: loading_more = false;
 	onMount(async () => {
 		page.subscribe(({ url }) => {
 			curr_page = Number(url.searchParams.get('page')) || 1;
@@ -39,4 +41,26 @@
 {#if is_ready}
 	<Carousel />
 	<RecentAnimePage {data} />
+	<!-- <div
+		class="w-full h-5 bg-red-600"
+		use:inview
+		on:inview_enter={() => {
+			loading_more = true;
+			fetchAnimes(curr_page + 1).then(({ results }) => {
+				console.log('Loading Page ' + curr_page);
+				// @ts-ignore
+				data.results = [...data.results, ...results];
+				curr_page++;
+				loading_more = false;
+			});
+		}}
+	/> -->
+
+	{#if loading_more}
+		<div class="font-bold absolute top-1/2 left-1/2 text-2xl h-10 bg-red-600">Loading Kbira</div>
+	{/if}
 {/if}
+
+<!-- <pre class="pre">
+	{JSON.stringify(data, null, 2)}
+</pre> -->
