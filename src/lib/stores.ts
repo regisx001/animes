f133bd7e8088c6a4b44e1e55d57b6e85f3e95f5d
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2024-04-13 04:24:03 +0000

    Themes: change default to Kokoro theme.

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,6 +3,6 @@ import { localStorageStore } from "@skeletonlabs/skeleton"
 
 export const searchStore: Writable<string | "no-data"> = writable("no-data")
 export const showSearchModalStore: Writable<boolean> = writable(false)
-export const themeStore: Writable<string> = localStorageStore("theme", "wintry")
+export const themeStore: Writable<string> = localStorageStore("theme", "kokoro")
 
 export const EpisodeLightStore: Writable<boolean> = writable(false)
